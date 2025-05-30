b654d7b8f9108944a43a040f0e2d29f117c40afe
Kemas Fachir Raihan <kemasfr.dev@gmail.com>
2025-05-30 22:36:00 +0700

    Dismissible to delete role data

--- lib/screens/manage_role.dart
+++ lib/screens/manage_role.dart
@@ -2,61 +2,105 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:workflow_shift/screens/add_role.dart';
 
-class ManageRole extends StatelessWidget {
+class ManageRole extends StatefulWidget {
   const ManageRole({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return StreamBuilder(
-      stream: FirebaseFirestore.instance
-          .collection('role')
-          .orderBy('title', descending: false)
-          .snapshots(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Center(
-            child: CircularProgressIndicator(),
-          );
-        }
-        if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-          return Center(
-            child: Text('No Data Found.'),
-          );
-        }
-        if (snapshot.hasError) {
-          return Center(
-            child: Text('Oops.. Something Wrong!'),
-          );
-        }
-        final dataRole = snapshot.data!.docs;
+  State<ManageRole> createState() => _ManageRoleState();
+}
 
-        return Scaffold(
-          appBar: AppBar(
-            title: Text('Manage Role'),
-            actions: [
-              IconButton(
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => AddRoleScreen(),
-                    ),
-                  );
-                },
-                icon: Icon(Icons.add),
-              ),
-            ],
+class _ManageRoleState extends State<ManageRole> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Manage Role'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => AddRoleScreen(),
+                ),
+              );
+            },
+            icon: Icon(Icons.add),
           ),
-          body: ListView.builder(
+        ],
+      ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('role')
+            .orderBy('title', descending: false)
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+            return Center(
+              child: Text('No Data Found.'),
+            );
+          }
+          if (snapshot.hasError) {
+            return Center(
+              child: Text('Oops.. Something Wrong!'),
+            );
+          }
+
+          final dataRole = snapshot.data!.docs;
+          return ListView.builder(
             padding: EdgeInsets.all(16),
             itemCount: dataRole.length,
             itemBuilder: (context, index) {
-              return ListTile(
-                title: Text(dataRole[index]['title']),
+              return Dismissible(
+                key: Key(dataRole[index].reference.id),
+                onDismissed: (direction) {
+                  final deletedRole = dataRole[index];
+                  dataRole[index].reference.delete();
+                  ScaffoldMessenger.of(context).clearSnackBars();
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('${deletedRole['title']} Deleted'),
+                      action: SnackBarAction(
+                        label: 'Undo',
+                        onPressed: () {
+                          setState(() {
+                            FirebaseFirestore.instance
+                                .collection('role')
+                                .add(deletedRole.data());
+                          });
+                        },
+                      ),
+                    ),
+                  );
+                },
+                background: Container(
+                  color: Colors.red.shade900,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.delete,
+                        color: Colors.white,
+                      ),
+                      Text(
+                        'Delete',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ],
+                  ),
+                ),
+                child: ListTile(
+                  title: Text(dataRole[index]['title']),
+                ),
               );
             },
-          ),
-        );
-      },
+          );
+        },
+      ),
     );
   }
 }

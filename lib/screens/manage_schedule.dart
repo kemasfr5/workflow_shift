9993f64bbd631acd6ea5061e63c7c45a25668e4b
Kemas Fachir Raihan <kemasfr.dev@gmail.com>
2025-06-10 19:53:54 +0700

    Date range on Manage Schedule

--- lib/screens/manage_schedule.dart
+++ lib/screens/manage_schedule.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:custom_date_range_picker/custom_date_range_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:workflow_shift/screens/add_schedule.dart';
 import 'package:workflow_shift/widgets/schedule_table_by_date.dart';
 
@@ -11,16 +13,24 @@ class ManageScheduleScreen extends StatefulWidget {
 }
 
 class _ManageScheduleScreenState extends State<ManageScheduleScreen> {
-  final now = DateTime.now();
+  late DateTime startDate;
+  late DateTime endDate;
+
+  @override
+  void initState() {
+    super.initState();
+
+    final now = DateTime.now();
+    startDate = DateTime(now.year, now.month, now.day);
+    endDate = DateTime(now.year, now.month, now.day + 30);
+  }
 
   @override
   void dispose() {
     super.dispose();
   }
 
-  Stream<QuerySnapshot> schedulesStream() {
-    var startDate = DateTime(now.year, now.month, now.day + 2);
-    var endDate = DateTime(now.year, now.month, now.day + 30);
+  Stream<QuerySnapshot> schedulesStream(DateTime startDate, DateTime endDate) {
     return FirebaseFirestore.instance
         .collection('schedule')
         .where('date', isGreaterThanOrEqualTo: startDate)
@@ -31,8 +41,8 @@ class _ManageScheduleScreenState extends State<ManageScheduleScreen> {
 
   @override
   Widget build(BuildContext context) {
-    // print(schedulesData);
-    var startDate = DateTime(now.year, now.month, now.day);
+    final f = DateFormat('yyyy-MM-dd');
+    final now = DateTime.now();
     return Scaffold(
         appBar: AppBar(
           title: Text('Manage Schedule'),
@@ -49,29 +59,64 @@ class _ManageScheduleScreenState extends State<ManageScheduleScreen> {
             )
           ],
         ),
-        body: StreamBuilder(
-          stream: schedulesStream(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-            if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-              return Center(
-                child: Text('Empty Data'),
-              );
-            }
-            return Container(
-              margin: EdgeInsets.all(16),
-              child: SingleChildScrollView(
-                child: ScheduleTableByDate(
-                  date: startDate,
-                  dataList: snapshot.data!.docs,
-                ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              ElevatedButton.icon(
+                  icon: Icon(Icons.calendar_month),
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue.shade100),
+                  onPressed: () {
+                    showCustomDateRangePicker(
+                      context,
+                      dismissible: true,
+                      minimumDate:
+                          DateTime.now().subtract(const Duration(days: 360)),
+                      maximumDate:
+                          DateTime.now().add(const Duration(days: 360)),
+                      endDate: startDate,
+                      startDate: startDate,
+                      backgroundColor: Colors.white,
+                      primaryColor: const Color.fromARGB(255, 0, 34, 94),
+                      onApplyClick: (start, end) {
+                        setState(() {
+                          endDate = end;
+                          startDate = start;
+                        });
+                      },
+                      onCancelClick: () {
+                        setState(() {
+                          startDate = DateTime(now.year, now.month, now.day);
+                          endDate = DateTime(now.year, now.month, now.day + 30);
+                        });
+                      },
+                    );
+                  },
+                  label: Text('${f.format(startDate)} - ${f.format(endDate)}')),
+              StreamBuilder(
+                stream: schedulesStream(startDate, endDate),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                  if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+                    return Center(
+                      child: Text('Empty Data'),
+                    );
+                  }
+                  return Container(
+                    margin: EdgeInsets.all(16),
+                    child: ScheduleTableByDate(
+                      date: startDate,
+                      dataList: snapshot.data!.docs,
+                    ),
+                  );
+                },
               ),
-            );
-          },
+            ],
+          ),
         ));
   }
 }
